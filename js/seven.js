c2637988d759b93708b207d1d9aa360525f4352e
yglee <yeongrace.lee@gmail.com>
2015-07-18 14:06:53 -0700

    jquery slider in search page

--- js/seven.js
+++ js/seven.js
@@ -1,4 +1,13 @@
-$('#foo').slider()
-    .on('slide', function(ev){
-        $('#bar').val(ev.value);
+$(function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 100,
+        values: [ 5, 30 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
     });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+});
