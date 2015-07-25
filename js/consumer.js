329d99f11fae4a1e91bb7796ea25fc58299448bb
yglee <yeongrace.lee@gmail.com>
2015-07-25 16:45:17 -0700

    debugging

--- js/consumer.js
+++ js/consumer.js
@@ -1,14 +1,17 @@
-$.ajax({url: 'http://foodnfellas.com/php/ConsumerProfile.php', dataType:'jsonp'});
-//$.ajax({
-//    url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
-//    crossDomain: true,
-//    dataType: 'jsonp',                //data format
-//    success: function(data)          //on recieve of reply
-//    {
-//
-//    },
-//    error: function(xhr, status, error) {
-//        var err = eval("(" + xhr.responseText + ")");
-//        alert(err.Message);
-//    }
-//});
+$(function(){
+    $.ajax({
+        url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
+        crossDomain: true,
+        //dataType: 'jsonp',                //data format
+        type: 'get',
+        success: function(data)          //on recieve of reply
+        {
+            console.log('success');
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+    });
+});
+
