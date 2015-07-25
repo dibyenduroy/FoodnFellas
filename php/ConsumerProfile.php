a390ea575f1262f33af612f73918d76e8605045e
yglee <yeongrace.lee@gmail.com>
2015-07-25 16:28:34 -0700

    very simple php

--- php/ConsumerProfile.php
+++ php/ConsumerProfile.php
@@ -1,19 +1,4 @@
 <?php header('content-type: application/json; charset=utf-8');
-
-session_start();
-$_SESSION["user_id"] = $_GET["userid"];
-#$session_user_id=$_SESSION["user_id"];
-$session_user_id=4;
-
-include('image_check.php');
-header('Cache-Control: no-cache');
-//
-
-getConsumerphoto();
-function getConsumerphoto() {
-
 $array_user_address = array(1,2,3,4);
-
 echo $_GET['callback'].'('.json_encode($array_user_address).');';
-}
 ?>
