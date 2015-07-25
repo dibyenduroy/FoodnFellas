9e4e12fcada132579e67adb847b68b0fa1b9dd7e
yglee <yeongrace.lee@gmail.com>
2015-07-25 15:31:01 -0700

    php debugging

--- js/consumer.js
+++ js/consumer.js
@@ -12,22 +12,26 @@ $(function ()
         {
             console.log("data is ", data);
             /*
-            var firstName = data[0];              //get id
-            var lastName = data[1];           //get name
-            var aboutMe = data[2];
-            var favoriteDish = data[3];
-            var email = data[4];
-            var phoneNumber = data[5];
-            //--------------------------------------------------------------------
-            // 3) Update provider content
-            //--------------------------------------------------------------------
-            $('firstName').value = firstName;
-            $('lastName').value = lastName;
-            $('aboutMe').value = aboutMe;
-            $('favoriteDish').value = favoriteDish;
-            $('email').value = email;
-            $('phoneNumber').value = phoneNumber;
-            */
+             var firstName = data[0];              //get id
+             var lastName = data[1];           //get name
+             var aboutMe = data[2];
+             var favoriteDish = data[3];
+             var email = data[4];
+             var phoneNumber = data[5];
+             //--------------------------------------------------------------------
+             // 3) Update provider content
+             //--------------------------------------------------------------------
+             $('firstName').value = firstName;
+             $('lastName').value = lastName;
+             $('aboutMe').value = aboutMe;
+             $('favoriteDish').value = favoriteDish;
+             $('email').value = email;
+             $('phoneNumber').value = phoneNumber;
+             */
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
         }
     });
 });
