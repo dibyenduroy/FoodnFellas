84c0adc58b55806a14e6236bdbcc7797b67b2eef
nidhimodh <nidhi.modh@gmail.com>
2015-07-25 15:33:22 -0700

    updates to search.php

--- php/search.php
+++ php/search.php
@@ -23,14 +23,14 @@ include 'foodnfellasDBConnection.php';
 // Get the entries from Provider_address matching the city.
 $sql1 = "SELECT pa.user_id, pa.provider_food_id FROM Provider_address  as pa JOIN Provider_food as pf 
                                                 ON pa.provider_food_id = pf.provider_food_id 
-                                                WHERE city = $city
-                                                AND pf.min_people <= $num_people 
-       											AND pf.max_people >= $num_people 
-       											AND pf.price_per_person BETWEEN $price_low AND $price_high 
-       											AND pf.meal_type = $meal_type 
-       											AND pf.cuisine_type = $cuisine_type 
-       											AND pf.delivery_method = $delivery_method 
-       											AND pf.provider_food_id = $loop_provider_food_id;";
+                                                WHERE city = ".$city."
+                                                AND pf.min_people <= ".$num_people." 
+       											                    AND pf.max_people >= ".$num_people."
+       											                    AND pf.price_per_person BETWEEN ".$price_low." AND ".$price_high." 
+       											                    AND pf.meal_type = ".$meal_type." 
+       											                    AND pf.cuisine_type = ".$cuisine_type." 
+       											                    AND pf.delivery_method = ".$delivery_method." 
+       											                    AND pf.provider_food_id = ".$loop_provider_food_id.";";
 $result1 = $conn->query($sql1);
 if ($result1->num_rows > 0) {	
 	// Now, return the meal entries. 
@@ -42,7 +42,7 @@ if ($result1->num_rows > 0) {
     		$sql2 = "SELECT m.dish_name, m.meal_description, m.meal_id, p.about_me, p.awards_won, p.kitchen_photo, p.food_album, p.cuisine_i_cook
     								FROM  Meal as m JOIN Provider as p 
     								ON m.user_id = p.user_id
-									WHERE  m.provider_food_id = $provider_id;";
+									WHERE  m.provider_food_id = ".$provider_id.";";
 
 			$result2 = $conn->query($sql2);
 			if ($result2->num_rows > 0) {
