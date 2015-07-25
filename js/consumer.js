e98f12863d6f3391a789b4849aaa3014896ed7e7
yglee <yeongrace.lee@gmail.com>
2015-07-25 16:29:41 -0700

    debugging

--- js/consumer.js
+++ js/consumer.js
@@ -4,24 +4,6 @@ $.ajax({
     dataType: 'jsonp',                //data format
     success: function(data)          //on recieve of reply
     {
-        //console.log("data is ", data);
-        /*
-         var firstName = data[0];              //get id
-         var lastName = data[1];           //get name
-         var aboutMe = data[2];
-         var favoriteDish = data[3];
-         var email = data[4];
-         var phoneNumber = data[5];
-         //--------------------------------------------------------------------
-         // 3) Update provider content
-         //--------------------------------------------------------------------
-         $('firstName').value = firstName;
-         $('lastName').value = lastName;
-         $('aboutMe').value = aboutMe;
-         $('favoriteDish').value = favoriteDish;
-         $('email').value = email;
-         $('phoneNumber').value = phoneNumber;
-         */
     },
     error: function(xhr, status, error) {
         var err = eval("(" + xhr.responseText + ")");
