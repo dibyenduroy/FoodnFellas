707219ce026a9485f90d284ee5477bd15439c81f
yglee <yeongrace.lee@gmail.com>
2015-07-25 16:20:11 -0700

    very simple php

--- js/consumer.js
+++ js/consumer.js
@@ -1,37 +1,30 @@
-$(function ()
-{    //-----------------------------------------------------------------------
-    // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
-    //-----------------------------------------------------------------------
-    $.ajax({
-        url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
-        crossDomain: true,
-        data: "",                        //you can insert url argumnets here to pass to api.php
-                                         //for example "id=5&parent=6"
-        dataType: 'jsonp',                //data format
-        success: function(data)          //on recieve of reply
-        {
-            console.log("data is ", data);
-            /*
-             var firstName = data[0];              //get id
-             var lastName = data[1];           //get name
-             var aboutMe = data[2];
-             var favoriteDish = data[3];
-             var email = data[4];
-             var phoneNumber = data[5];
-             //--------------------------------------------------------------------
-             // 3) Update provider content
-             //--------------------------------------------------------------------
-             $('firstName').value = firstName;
-             $('lastName').value = lastName;
-             $('aboutMe').value = aboutMe;
-             $('favoriteDish').value = favoriteDish;
-             $('email').value = email;
-             $('phoneNumber').value = phoneNumber;
-             */
-        },
-        error: function(xhr, status, error) {
-            var err = eval("(" + xhr.responseText + ")");
-            alert(err.Message);
-        }
-    });
+$.ajax({
+    url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
+    crossDomain: true,
+    dataType: 'jsonp',                //data format
+    success: function(data)          //on recieve of reply
+    {
+        console.log("data is ", data);
+        /*
+         var firstName = data[0];              //get id
+         var lastName = data[1];           //get name
+         var aboutMe = data[2];
+         var favoriteDish = data[3];
+         var email = data[4];
+         var phoneNumber = data[5];
+         //--------------------------------------------------------------------
+         // 3) Update provider content
+         //--------------------------------------------------------------------
+         $('firstName').value = firstName;
+         $('lastName').value = lastName;
+         $('aboutMe').value = aboutMe;
+         $('favoriteDish').value = favoriteDish;
+         $('email').value = email;
+         $('phoneNumber').value = phoneNumber;
+         */
+    },
+    error: function(xhr, status, error) {
+        var err = eval("(" + xhr.responseText + ")");
+        alert(err.Message);
+    }
 });
