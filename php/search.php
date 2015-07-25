8c2b340ad331a0c2034ee9bd1464b4aec7538d8d
nidhimodh <nidhi.modh@gmail.com>
2015-07-25 13:59:35 -0700

    added search.php and search_example.php

---
+++ php/search.php
@@ -0,0 +1,65 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+
+
+$return = $_POST;
+
+$user_id = $return["user_id"];
+// This will be used in Provider_address table.
+$city = $return["city"];
+
+// These fields will be used in Provider_food table.
+$num_people = $return["num_people"];
+$date = $return["date"];
+$price_low = $return["price_low"];
+$price_high = $return["price_high"];
+$meal_type = $return["meal_type"];
+$cuisine_type = $return["cuisine_type"];
+$delivery_method = $return["delivery_method"];
+
+include 'foodnfellasDBConnection.php';
+
+// Get the entries from Provider_address matching the city.
+$sql1 = "SELECT pa.user_id, pa.provider_food_id FROM Provider_address  as pa JOIN Provider_food as pf 
+                                                ON pa.provider_food_id = pf.provider_food_id 
+                                                WHERE city = $city
+                                                AND pf.min_people <= $num_people 
+       											AND pf.max_people >= $num_people 
+       											AND pf.price_per_person BETWEEN $price_low AND $price_high 
+       											AND pf.meal_type = $meal_type 
+       											AND pf.cuisine_type = $cuisine_type 
+       											AND pf.delivery_method = $delivery_method 
+       											AND pf.provider_food_id = $loop_provider_food_id;";
+$result1 = $conn->query($sql1);
+if ($result1->num_rows > 0) {	
+	// Now, return the meal entries. 
+	while($row = $result1->fetch_assoc()) {
+			$user_id = row["pa.user_id"];
+			$provider_id = row["pa.provider_food_id"];
+
+
+    		$sql2 = "SELECT m.dish_name, m.meal_description, m.meal_id, p.about_me, p.awards_won, p.kitchen_photo, p.food_album, p.cuisine_i_cook
+    								FROM  Meal as m JOIN Provider as p 
+    								ON m.user_id = p.user_id
+									WHERE  m.provider_food_id = $provider_id;";
+
+			$result2 = $conn->query($sql2);
+			if ($result2->num_rows > 0) {
+				$array_meal_search_row = mysql_fetch_row($result2);
+			}
+
+			$array_meal_search_all = array_merge($array_meal_search_all, $array_meal_search_row);
+    }
+} else {
+    echo "0 results";
+}
+
+// At this point, we have all the information for the search query.
+echo json_encode($array_meal_search_all);
+
+
+$conn->close();
+}
+
+?>
---
+++ php/search_example.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Upload Files to Amazon S3 PHP</title>
+</head>
+
+<body>
+
+<h4> Order Transaction Page <h>   
+<form action="search.php" method='post' enctype="multipart/form-data">
+
+
+	<br>
+   <br> <input type="text" name="user_id" value="" placeholder="user_id" />  <br>
+   <br> <input type="text" name="city" value="" placeholder="city" />  <br>
+   <br> <input type="text" name="num_people" value="" placeholder="num_people" />  <br>
+   <br> <input type="text" name="date" value="" placeholder="date" /> <br>
+   <br> <input type="text" name="price_low" value="" placeholder="price_low" />  <br>
+   <br> <input type="text" name="price_high" value="" placeholder="price_high" />  <br>
+   <br> <input type="text" name="meal_type" value="" placeholder="meal_type" />  <br>
+   <br> <input type="text" name="cuisine_type" value="" placeholder="cuisine_type" />  <br>
+   <br> <input type="text" name="delivery_method" value="" placeholder="delivery_method" />  <br>
+   
+    <br>
+
+<input type='submit' value='Update Details' onClick="window.location.reload(true)"/></div>
+</form>
+
+		
+
+</body>
+</html>
