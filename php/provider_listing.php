32585731f478df0afd366cc08692a36602fbd3de
nidhimodh <nidhi.modh@gmail.com>
2015-07-26 14:10:14 -0700

    debugging info to provider_listing.php

--- php/provider_listing.php
+++ php/provider_listing.php
@@ -9,7 +9,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 
 	include 'foodnfellasDBConnection.php';
 
-	$sql1 = "SELECT u.f_name, u.l_name, u.photo, p.about_me, p.kitchen_photo, p.awards_won, p.cuisines_i_cook 
+	$sql1 = "SELECT u.f_name, u.l_name, u.photo, u.about_me, p.kitchen_photo, p.awards_won, p.cuisine_i_cook 
 	                FROM User as u 
 	                JOIN Provider as p 
 	                ON u.user_id = p.user_id
@@ -39,6 +39,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 
 	$result2 = $conn->query($sql2);
 	if ($result2->num_rows > 0) {
+		echo " if2 success ";
 		while ($row = $result2->fetch_assoc()) {
 			$array_row = $result2->fetch_assoc();
 			print_r($array_row);
