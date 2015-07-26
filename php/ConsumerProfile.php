f431fc5581921d2b5efa1af4b538022bb614af29
yglee <yeongrace.lee@gmail.com>
2015-07-25 17:36:08 -0700

    testing

--- php/ConsumerProfile.php
+++ php/ConsumerProfile.php
@@ -1,11 +1,7 @@
 <?php
-ob_start();
 header('content-type: text/javascript;');
 $array_user_address = array(1,2,3,4);
 
 // wrap the data as with the callback
-$callback = isset($_GET["callback"]) ? $_GET["callback"] : "alert";
-ob_clean();
 echo $_GET['callback'] . "(".$array_user_address.");";
-ob_flush();
 ?>
