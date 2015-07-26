a1d8d14c5d97e15ea1d43cebececbe129fd817bb
nidhimodh <nidhi.modh@gmail.com>
2015-07-25 23:53:47 -0700

    updated search.php query: added unit test code

--- php/search.php
+++ php/search.php
@@ -18,42 +18,55 @@ $meal_type = $return["meal_type"];
 $cuisine_type = $return["cuisine_type"];
 $delivery_method = $return["delivery_method"];
 
-include 'foodnfellasDBConnection.php';
+//include 'foodnfellasDBConnection.php';
+$servername = "fm1s2t7e010rjki.cnn0dbzvr04c.us-west-2.rds.amazonaws.com";
+$username = "fnfsandbox";
+$password = "greatfood123";
+$dbname = "foodnfellas";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "Error Connecting to Database";
+}
 
 // Get the entries from Provider_address matching the city.
 $sql1 = "SELECT `pa.user_id`, `pa.provider_food_id` FROM `Provider_address`  as pa JOIN `Provider_food` as pf 
                                                 ON `pa.provider_food_id` = `pf.provider_food_id` 
-                                                WHERE `city` = '".$city."';"; 
+                                                WHERE `pa.city` = '".$city."';"; 
 //                                                AND pf.min_people <= ".$num_people." 
 //                                                AND pf.max_people >= ".$num_people."
 //                                                AND pf.price_per_person BETWEEN ".$price_low." AND ".$price_high." 
 //                                                AND pf.meal_type = ".$meal_type." 
 //                                                AND pf.cuisine_type = ".$cuisine_type." 
 //                                                AND pf.delivery_method = ".$delivery_method." 
-
+//if (($conn->query($sql1) === TRUE)) {
 $result1 = $conn->query($sql1);
-echo "HI";
-echo $result1;
+  echo "HI";
+
 if ($result1->num_rows > 0) {	
+  echo "I am inside result if ";
 	// Now, return the meal entries. 
 	while($row = $result1->fetch_assoc()) {
+      echo "I am inside result while ";
 			$provider_id = $row["pa.provider_food_id"];
-
     	$sql2 = "SELECT m.dish_name, m.meal_description, m.meal_id, p.about_me, p.awards_won, p.kitchen_photo, p.food_album, p.cuisine_i_cook
     								FROM  Meal as m JOIN Provider as p 
     								ON m.user_id = p.user_id
 									WHERE  m.provider_food_id = ".$provider_id.";";
-
 			$result2 = $conn->query($sql2);
 			if ($result2->num_rows > 0) {
 				$array_meal_search_row = mysql_fetch_row($result2);
 			}
-
-			$array_meal_search_all = array_merge($array_meal_search_all, $array_meal_search_row);
+			//$array_meal_search_all = array_merge($array_meal_search_all, $array_meal_search_row);
   }
 } else {
     echo "0 results";
-}
+    echo "The is some issue in conn->query";
+} 
+ 
 
 // At this point, we have all the information for the search query.
 //echo json_encode($array_meal_search_all);
