3b637b4093aa230712537f1ea44401cb618ef601
yglee <yeongrace.lee@gmail.com>
2015-07-25 19:39:58 -0700

    php integration

--- php/ConsumerProfile.php
+++ php/ConsumerProfile.php
@@ -26,7 +26,7 @@ function getConsumerphoto() {
         echo "Error Connecting to Database";
     }
 
-    $queryUserInfo = "SELECT `user_id`,`photo`,`about_me`,`my_fav_dish`,`phone_number`,`f_name`,`l_name`,`email` FROM `user`";
+    $queryUserInfo = "SELECT `user_id`,`photo`,`about_me`,`my_fav_dish`,`phone_number`,`f_name`,`l_name`,`email` FROM `user`;";
 
     $array_user_info = Array();
     $index = 0;
