c3f0f6c10a333053203c852a23d24b80f98b5dd5
nidhimodh <nidhi.modh@gmail.com>
2015-07-26 14:26:06 -0700

    ProviderProfile.pho  1

--- php/ProviderProfile.php
+++ php/ProviderProfile.php
@@ -28,37 +28,51 @@ $fetch_provider_info = "select u.user_id as user_id,u.photo as photo,p.about_me 
 $result = $conn->query($fetch_provider_info);
 //$array_user_info = $result->fetch_assoc();
 //mysql_data_seek($result, 0);
+$array_info_address = Array();
+$index = 0;
 
  if ($result->num_rows > 0) {
+    echo "i am inside if               -";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      $s3_kitchen_photo=$row["kitchen_photo"];
-      $s3_food_photo=$row["food_album"];
-      $s3_provider_photo=$row["photo"];
+        $array_info_address[$index] = $row2;
+        $index++;
+        echo "I am inside while             -;";
+        $s3_kitchen_photo=$row["kitchen_photo"];
+        $s3_food_photo=$row["food_album"];
+        $s3_provider_photo=$row["photo"];
 
     echo " The User ID is  : ".$row["user_id"] ."<br>";
     echo " About Me : ".$row["about_me"] ."<br>";
     echo " Cuisines I cook : ".$row["cuisine_i_cook"] ."<br>";
     echo " Awards won : ".$row["awards_won"] ."<br><br>";
-    //echo " Provider Photo URL  : ".$row["photo"]."<br><br>";
-    //echo " Provider Kitchen URL  : ".$row["kitchen_photo"]."<br><br>";
-    //echo " Provider Food URL  : ".$row["food_album"]."<br><br>";
+    echo " Provider Photo URL  : ".$row["photo"]."<br><br>";
+    echo " Provider Kitchen URL  : ".$row["kitchen_photo"]."<br><br>";
+    echo " Provider Food URL  : ".$row["food_album"]."<br><br>";
     echo " Provider Image : <img  float:left' SRC='$s3_provider_photo'> <br>" ;
     echo " Provider KItchen : <img  float:left' SRC='$s3_kitchen_photo'> <br>" ;
     echo " Food Served Today :<img  SRC='$s3_food_photo'> <br>" ;
     }
-} else {
+} else {    
     echo "0 results";
 }
 
 $fetch_provider_address = "SELECT address_id, country, state, zip_code, city, street_1, street_2, house_no, address_name  FROM Provider_address where user_id = '4';";
 $result2 = $conn->query($fetch_provider_address);
-$array_user_address = mysql_fetch_row($result2)
-mysql_data_seek($result2, 0);
+//$array_user_address = mysql_fetch_row($result2)
+//mysql_data_seek($result2, 0);
+
+
+
 
 if ($result2->num_rows > 0) {
+    echo "I am inside if 2            -";
   // output data of each row
   while($row2 = $result2->fetch_assoc()) {
+
+    $array_info_address[$index] = $row2;
+    $index++;
+    echo "I am insdie while 2           -";
     echo "<br>";
     echo "<br>";
     echo "<br>";
@@ -77,9 +91,6 @@ if ($result2->num_rows > 0) {
     echo "0 results";
 }
 $all_user_info = array_merge($array_user_info, $array_user_address);
-echo json_encode($all_user_info);
-
+echo $_GET['callback'].'('.json_encode($array_user_address).');';
 }
-
-
 ?>
