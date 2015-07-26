1a7e3910be93447f2713a020f6bd1abcca881c23
nidhimodh <nidhi.modh@gmail.com>
2015-07-25 23:55:54 -0700

    updated search.php query: added sql print

--- php/search.php
+++ php/search.php
@@ -44,6 +44,7 @@ $sql1 = "SELECT `pa.user_id`, `pa.provider_food_id` FROM `Provider_address`  as 
 //                                                AND pf.delivery_method = ".$delivery_method." 
 //if (($conn->query($sql1) === TRUE)) {
 $result1 = $conn->query($sql1);
+  echo "$sql1";
   echo "HI";
 
 if ($result1->num_rows > 0) {	
