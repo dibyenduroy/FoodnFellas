39bf6607f5c2389899cb14db6a6496c82813afe1
yglee <yeongrace.lee@gmail.com>
2015-07-25 17:20:17 -0700

    debugging

--- js/consumer.js
+++ js/consumer.js
@@ -1,19 +1,14 @@
 jQuery(
     $(function(){
-    $.ajax({
-        url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
-        crossDomain: true,
-        dataType: 'jsonp',                //data format
-        type: 'GET',
-        success: function(data)          //on recieve of reply
-        {
-            console.log(JSON.stringify(data))
-            console.log('success');
-        },
-        error: function(xhr, status, error) {
-            var err = eval("(" + xhr.responseText + ")");
-            alert(err.Message);
-        }
-    });
+        $.ajax({
+            url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
+            crossDomain: true,
+            dataType: 'jsonp',                //data format
+            type: 'GET'
+        }).done(function(response){
+                console.log(response);
+        }).fail(function(error){
+            console.log(error.statusText);
+        });
     })
 );
