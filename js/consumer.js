bb660c5d99ad1bd06c726da735dead6e7cf67e41
yglee <yeongrace.lee@gmail.com>
2015-07-26 11:08:49 -0700

    logout button

--- js/consumer.js
+++ js/consumer.js
@@ -16,8 +16,14 @@ jQuery(
                     'data-target=".social-signup-modal">Sign Up</a></li>');
             } else {
                 // Show user ID in the navbar
+                $("#userNameOrLogin").prepend('<button type="button" class="btn btn-default">
+                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span> Star
+                    </button>'');
                 $("#userNameOrLogin").prepend('<li id="userId"> Signed in as '+ data["user_id"] +'</li>');
 
+                //add logout btn
+
+                // fill in the form with existing user data.
                 $('#firstName').val("dummy first name");
                 $('#lastName').val("dummy last name");
                 $('#aboutMe').val(data['about_me']);
@@ -25,6 +31,7 @@ jQuery(
                 $('#email').val("dummy email");
                 $('#phoneNumber').val(data['phone_number']);
 
+                // display user profile photo
                 if (data['photo']) {
                     //replace the photo
                     var image = $('#profile_image')[0];
