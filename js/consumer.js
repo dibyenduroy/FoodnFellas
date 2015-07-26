28b09cc85130cded3a4d06de4e0bc485348ec928
yglee <yeongrace.lee@gmail.com>
2015-07-25 17:38:33 -0700

    testing

--- js/consumer.js
+++ js/consumer.js
@@ -1,22 +1,19 @@
-$.getJSON('http://foodnfellas.com/php/ConsumerProfile.php?callback=?','',function(data){
-    alert(data);
-});
-//jQuery(
-//    $(function(){
-//    $.ajax({
-//        url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
-//        crossDomain: true,
-//        dataType: 'jsonp',                //data format
-//        type: 'GET',
-//        success: function(data)          //on recieve of reply
-//        {
-//            console.log(JSON.stringify(data))
-//            console.log('success');
-//        },
-//        error: function(xhr, status, error) {
-//            var err = eval("(" + xhr.responseText + ")");
-//            alert(err.Message);
-//        }
-//    });
-//    })
-//);
+jQuery(
+    $(function(){
+    $.ajax({
+        url: 'http://foodnfellas.com/php/ConsumerProfile.php',                  //the script to call to get data
+        crossDomain: true,
+        dataType: 'jsonp',                //data format
+        type: 'GET',
+        success: function(data)          //on recieve of reply
+        {
+            console.log(JSON.stringify(data))
+            console.log('success');
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+    });
+    })
+);
--- php/ConsumerProfile.php
+++ php/ConsumerProfile.php
@@ -3,5 +3,4 @@ header('content-type: text/javascript;');
 $array_user_address = array(1,2,3,4);
 
 // wrap the data as with the callback
-echo $_GET['callback'] . "(".$array_user_address.");";
-?>
+echo $_GET['callback'].'('.json_encode($array_user_address).');';
