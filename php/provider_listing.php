382aefe7c38799ea0b9daf60b6016ac9f937d5f1
nidhimodh <nidhi.modh@gmail.com>
2015-07-26 13:19:36 -0700

    unit test provider listings

--- php/provider_listing.php
+++ php/provider_listing.php
@@ -12,11 +12,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 	                ON u.user_id = p.user_id
 	                WHERE u.user_id = '".$user_id."';";
 
+	echo " SQL1    ";
+	echo $sql1;
+
 	$result1 = $conn->query($sql1);
 	if ($result1->num_rows > 0) {
+		echo "  if1 success   ";
 		$array_unique_result = $result1->fetch_assoc();
 	} else {
-		echo "0 results";
+		echo "0 results1";
 	}
 
 	$sql2 = "SELECT p.food_album, m.dish_name, m.meal_description, pf.price_per_person, pf.delivery_method, pf.meal_type, 
@@ -27,18 +31,23 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
                                  JOIN Provider as p
                                  ON pf.user_id = p.user_id
                                  WHERE m.user_id = '".$user_id."';";
+    echo "  SQL2   ";
+    echo sql2;
 
 	$result2 = $conn->query($sql2);
 	if ($result2->num_rows > 0) {
 		while ($row = $result2->fetch_assoc()) {
 			$array_row = $result2->fetch_assoc();
+			print_r($array_row);
 		} 
 		$array_repeated_results = array_merge($array_repeated_results, $array_row);
 	} else {
-		echo "0 results";
+		echo "0 results2";
 	}
 
 	$provider_listings = array_merge($array_unique_result, $array_repeated_results);
+	echo "  printing provider listings   ";
+	print_r(provider_listings);
 
 	// At this point, we have all the information for the search query.
 	echo json_encode($provider_listings);
