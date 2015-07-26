1635f440a4120710f1a3bd661516e32e196837b4
yglee <yeongrace.lee@gmail.com>
2015-07-26 10:54:30 -0700

    when logged in, display user info

--- js/consumer.js
+++ js/consumer.js
@@ -9,26 +9,29 @@ jQuery(
         {
             var data = data[0];
 
-            //if (!$('#user_id')) {
-            if (true) {
+            if (!data['user_id']) { //if no user id, show blank page with sign up and login btns
                 $("#userNameOrLogin").prepend('<li><a href="/login" data-toggle="modal" '+
                     'data-target=".social-login-modal">Login</a></li>');
                 $("#userNameOrLogin").prepend('<li><a href="/signup" data-toggle="modal" '+
                     'data-target=".social-signup-modal">Sign Up</a></li>');
-            }
-            $('#firstName').val("dummy first name");
-            $('#lastName').val("dummy last name");
-            $('#aboutMe').val(data['about_me']);
-            $('#favoriteDish').val(data['my_fav_dish']);
-            $('#email').val("dummy email");
-            $('#phoneNumber').val(data['phone_number']);
+            } else {
+                // Show user ID in the navbar
+                $("#userNameOrLogin").prepend('<div id="userId"> data['user_id'] </div>');
 
-            if (data['photo']) {
-                //replace the photo
-                var image = $('#profile_image')[0];
-                image.src = data['photo'];
-                image.style.height = '200px';
-                image.style.width = '200px';
+                $('#firstName').val("dummy first name");
+                $('#lastName').val("dummy last name");
+                $('#aboutMe').val(data['about_me']);
+                $('#favoriteDish').val(data['my_fav_dish']);
+                $('#email').val("dummy email");
+                $('#phoneNumber').val(data['phone_number']);
+
+                if (data['photo']) {
+                    //replace the photo
+                    var image = $('#profile_image')[0];
+                    image.src = data['photo'];
+                    image.style.height = '200px';
+                    image.style.width = '200px';
+                }
             }
         },
         error: function(xhr, status, error) {
