32e8de6e49467299fc0d89b482f64aee24541ba3
nidhimodh <nidhi.modh@gmail.com>
2015-07-26 17:57:22 -0700

    Added some debug prints

--- php/provider_listing.php
+++ php/provider_listing.php
@@ -32,6 +32,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 		echo "  if1 success   ";
 		$array_unique_result = $result1->fetch_assoc();
 		echo "  sql1 result  ";
+		echo "  Printing array_unique_result  "."<br>";
 		print_r($array_unique_result);
 	} else {
 		echo "0 results1";
@@ -46,26 +47,31 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
                                  ON pf.user_id = p.user_id
                                  WHERE m.user_id = '".$user_id."';";
     // Delete these echos during final integration.
-    echo "  SQL2   ";
-    echo sql2;
+    echo "<br>"."  SQL2   ";
+    echo $sql2;
 
 	$result2 = $conn->query($sql2);
 	if ($result2->num_rows > 0) {
 		// Delete these echos and print_r during final integration.
-		echo " if2 success ";
+		echo "<br>"." if2 success ";
 		while ($row = $result2->fetch_assoc()) {
 			$array_row = $result2->fetch_assoc();
+			echo "<br>"."printing each repeated result";
 			print_r($array_row);
+			$array_repeated_results = array_merge($array_repeated_results, $array_row);	
 		} 
-		$array_repeated_results = array_merge($array_repeated_results, $array_row);
+		
 	} else {
 		echo "0 results2";
 	}
 
+	echo "<br>"."  Printing array_repeated_results  ";
+	print_r($array_repeated_results);
+
 	$provider_listings = array_merge($array_unique_result, $array_repeated_results);
 	// Delete these echos and print_r during final integration.
-	echo "  printing provider listings    ";
-	// print_r($provider_listings);
+	echo "<br>"."  printing provider listings    ";
+	print_r($provider_listings);
 
 	// At this point, we have all the information for the search query.
 	// wrap the data as with the callback
