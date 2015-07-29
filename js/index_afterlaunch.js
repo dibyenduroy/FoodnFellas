0c5db200a4d7f8d42e98dfc8f7af9f24bbaf5c34
yglee <yeongrace.lee@gmail.com>
2015-07-28 19:52:08 -0700

    sign up and redirect to home on success

--- js/index_afterlaunch.js
+++ js/index_afterlaunch.js
@@ -4,32 +4,34 @@ function providerBtnCallBack() {
     location.href='/html/provider.html';
 }
 
+//function redirectToHomePage() {
+//    // get the user id back from the registration.php
+//    // get the user data back from the consumer.php (see how consumer.js) does this
+//    // if the user data exists, in database
+//    // then reload index_afterlaunch.html with the
+//    //login info
+//
+//    //location.href='index_afterlaunch.html';
+//}
+
 function redirectToHomePage() {
-    // get the user id back from the registration.php
-    // get the user data back from the consumer.php (see how consumer.js) does this
-    // if the user data exists, in database
-    // then reload index_afterlaunch.html with the
-    //login info
-
-    //location.href='index_afterlaunch.html';
-}
-
-jQuery(
-    $(function(){
-        $.ajax({
-            url: '../php/registration.php',                  //the script to call to get data
-            crossDomain: true,
-            dataType: 'jsonp',                //data format
-            type: 'GET',
-            success: function(data)
-            {
-                console.log('inside success');
-                console.log(data);
-            },
-            error: function(xhr, status, error) {
-                var err = eval("(" + xhr.responseText + ")");
-                alert(err.Message);
-            }
-        });
-    })
+    jQuery(
+        $(function(){
+            $.ajax({
+                url: '../php/registration.php',   //the script to call to get data
+                crossDomain: true,
+                dataType: 'jsonp',                //data format
+                type: 'GET',
+                success: function(data)
+                {
+                    console.log('inside success');
+                    console.log(data);
+                },
+                error: function(xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    alert(err.Message);
+                }
+            });
+        })
+    );
 );
