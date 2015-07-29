0badeec4bb8521d0b1aaa07abd57a39fdce2f4d4
yglee <yeongrace.lee@gmail.com>
2015-07-29 00:55:12 -0700

    original consumer profile page saved as ConsumerProfile_orig.php

--- js/index_afterlaunch.js
+++ js/index_afterlaunch.js
@@ -46,19 +46,23 @@ function signupCallBack() {
 }
 
 function loginCallBack() {
-    $.ajax({
-        url: './php/login_v2.php',   //the script to call to get data
-        crossDomain: true,
-        dataType: 'jsonp',                //data format
-        type: 'GET',
-        success: function (data) {
-            console.log('inside success');
-            console.log(data);
-            alert("Successful Login");
-        },
-        error: function (xhr, status, error) {
-            var err = eval("(" + xhr.responseText + ")");
-            alert("Unsuccessful login. Please try again");
-        }
-    });
+    jQuery(
+        $(function () {
+            $.ajax({
+                url: './php/login_v2.php',   //the script to call to get data
+                crossDomain: true,
+                dataType: 'jsonp',                //data format
+                type: 'GET',
+                success: function (data) {
+                    console.log('inside success');
+                    console.log(data);
+                    alert("Successful Login");
+                },
+                error: function (xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    alert("Unsuccessful login. Please try again");
+                }
+            });
+        })
+    );
 }
