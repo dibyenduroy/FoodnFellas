074556ddced2c2c7adfbf05f434e2424a9df009c
nidhimodh <nidhi.modh@gmail.com>
2015-08-02 14:38:06 -0700

    Fixed logout.php and added cookie destroy

--- php/logout.php
+++ php/logout.php
@@ -1,11 +1,11 @@
-<?php include_once("db.php"); ?>
-
 <?php
 
 session_unset(); #removes all the variables in the session
 session_destroy(); # destroys the session
+// Destroy the cookie.
+unset($_COOKIE['FoodnFellas']);
 
-if (!$_SESSION['userName'])
+if (!$_SESSION['user_id'])
 	echo "Successfully logged out!";
 else
 	echo "Error Occurred!! <br />";
