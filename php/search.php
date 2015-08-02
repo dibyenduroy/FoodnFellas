ab1e0bdc4f4435852d311b115501bbfd0276ee6d
nidhimodh <nidhi.modh@gmail.com>
2015-08-01 18:30:38 -0700

    fixing search 21

--- php/search.php
+++ php/search.php
@@ -148,7 +148,7 @@
             <div class="col-md-3">
                 <label for="Name">By: </label>
                 <output name="AboutMe" id ="AboutMe">
-                <label for="price_per_person">Price per person: <?php echo $array_meal_search_all[$i];?></label>
+                <label for="price_per_person">Price per person: <?php echo $i;?></label>
                 <output name="price_per_person" id ="price_per_person">
             </div>
           <?php endfor; ?>
