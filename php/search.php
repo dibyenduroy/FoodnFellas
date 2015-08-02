1e90c8f2b53b75b10a99c3b6127f689d1ba05bd2
nidhimodh <nidhi.modh@gmail.com>
2015-08-01 21:39:25 -0700

    fixing search 33

--- php/search.php
+++ php/search.php
@@ -120,7 +120,7 @@
                   echo ("<label for='Name'>By: ".$array_meal_search_row['f_name']." ".$array_meal_search_row['l_name']."  "."</label>");
                   echo ("<label for='price_per_person'>Price per person: ".$array_meal_search_row['price_per_person']."</label></div>");*/
                   ?>
-              <div class="col-md-6">
+              <div class="col-md-3">
                 <img class="img-responsive" id="photo" src="http://placehold.it/150x150" alt="">
             </div>
             <div class="col-md-3">
