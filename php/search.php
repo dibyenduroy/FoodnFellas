79a67c8ba7f81193911460ed04c3fdf1e383d758
nidhimodh <nidhi.modh@gmail.com>
2015-08-01 22:18:52 -0700

    fixing search 33

--- php/search.php
+++ php/search.php
@@ -115,13 +115,15 @@
                 $index++; 
                 //print_r($array_meal_search_row);
                 $counter++; 
+                $pfid = $array_meal_search_row['provider_food_id'];
+                echo $pfid;
                 /*echo("<div>".$array_meal_search_row['photo']."</div>");
                   echo ("<div class='col-md-6'><img class='img-responsive' id='photo' src=".$array_meal_search_row['photo']." alt=''></div><div class='col-md-3'>");
                   echo ("<label for='Name'>By: ".$array_meal_search_row['f_name']." ".$array_meal_search_row['l_name']."  "."</label>");
                   echo ("<label for='price_per_person'>Price per person: ".$array_meal_search_row['price_per_person']."</label></div>");*/
                   ?>
               <div class="col-md-6">
-                <?php $pfid = $array_meal_search_row['provider_food_id']; ?>
+      
                 
                 <a href="../php/search_results.php?pfid=<?php $pfids?>"><img class="img-responsive" id="photo" src="http://placehold.it/150x150" alt=""></a>
                 <label for="Name">By:<?php echo $array_meal_search_row['f_name']." ".$array_meal_search_row['l_name']."  "; ?> </label>
