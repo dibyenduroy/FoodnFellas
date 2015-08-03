ad0820dfe47ae9a89e88a3b0c075672dd30b61e7
dibyenduroy <connect2d_roy@yahoo.co.in>
2015-08-02 22:27:10 -0700

    Update again

--- php/search_results.php
+++ php/search_results.php
@@ -175,11 +175,11 @@ $conn->close();
   <br> <input type="hidden" name="provider_id" value="7" placeholder="provider_id" />  <br>
    <br> <input type="hidden" name="consumer_id" value="38" placeholder="consumer_id" />  <br>
    <br> <input type="hidden" name="meal_id" value="123" placeholder="meal_id" />  <br>
-   <br> <input type="text" name="cost" value="120" placeholder="cost" /> <br>
-   <br> <input type="hidden" name="DeliveryMethod" value="PICKUP" placeholder="DeliveryMethod" />  <br>
-   <br> <input type="text" name="NumberofAdult" value="" placeholder="NumberofAdult" />  <br>
-   <br> <input type="text" name="NumberofKids" value="" placeholder="NumberofKids" />  <br>
-   <br> <input type="text" id="SpecialNote" name="SpecialNote" value="" style="line-height: 5em;" placeholder="SpecialNote"/>  <br>
+   <br> Cost <input type="text" name="cost" value="120" placeholder="cost" readonly/> <br>
+   <br>  Delivery Method <input type="text" name="DeliveryMethod" value="PICKUP" placeholder="DeliveryMethod" readonly />  <br>
+   <br> Number of Adults <input type="text" name="NumberofAdult" value="20" placeholder="NumberofAdult" readonly/>  <br>
+   <br> NUmber of Kids <input type="text" name="NumberofKids" value="0" placeholder="NumberofKids"  readonly/>  <br>
+   <br> Special Note <input type="text" id="SpecialNote" name="SpecialNote" value="" style="line-height: 5em;" placeholder="SpecialNote"/>  <br>
    <br> <input type="hidden" name="ReviewID" value="123" placeholder="ReviewID" />  <br>
     <button type="submit" class="btn btn-danger btn-lg btn-block" onClick="window.location.reload(true)">Order</button>           
   </form>   
